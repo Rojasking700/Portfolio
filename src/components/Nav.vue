b172a9466253464f270a7111906d384a853d7a8d
Rojasking700 <grojaspiano@gmail.com>
2021-08-11 21:26:02 -0500

    installing router

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,33 @@
 <template>
-    <nav class="nav">
-        <h1 id="logo">
-            <img src="../assets/crown.png" >
-             Rojasking
-             </h1>
-        <ul class="nav-links">
-            <li>Home</li>
-            <li>Contact</li>
-            <li>About</li>
-        </ul>   
-    </nav>
+    <header :class="{ 'scrolled-nav' : scrollPosition }">
+        <div class="branding">
+            <img src="../assets/crown.png" alt="">
+        </div>
+        <ul v-show="!mobile" class="navigation">
+            <li><router-link class="link" :to="{}"> </router-link></li>
+        </ul>
+    </header>
 </template>
 
+<script>
+export default {
+    // setup() {
+    //     const myFunction = () => {
+    //         var x = document.getElementsById("nav");
+    //         if (x.className ==="nav"){
+    //             x.className += "responsive";
+    //         }else {
+    //             x.className = 'nav';
+    //         }
+    //     }
+
+    //     return {myFunction}
+    // }
+}
+</script>
+
 <style>
-    .nav {
+    /* .nav {
         padding: 3rem;
         position: absolute;
         width: 100%;
@@ -25,10 +39,17 @@
         color: #fff;
         text-shadow: 2px 2px black;
     }
+    .nav a {
+        color:#fff;
+        text-decoration: none;
+        padding-left: 10rem;
+        font-size: 1.2rem;
+        display: flex;
+    }
     nav img {
-        height:2vw;
+        height:2rem;
         transform: rotate(-30deg);
-        
+
     }
     .nav-links {
         display: flex;
@@ -38,5 +59,31 @@
         padding-left: 10rem;
         font-size: 1.2rem;
     }
+    .nav .icon{
+        display: none;
+    }
+
+    @media (max-width: 800px) {
+        .nav a { display: none;}
+        .nav a.icon{
+            display: block;
+            float:right;
+        }
+    }
+    @media (max-width: 800px) {
+        .nav.responsive { position: relative;}
+        .nav.responsive .icon {
+            position: absolute;
+            right: 0;
+            top: 0;
+        }
+        .nav.responsive a{
+            float: none;
+            display: block;
+            text-align: left;
+        }
+    }
+     */
 
 </style>
+
