751555dac70343963ff0f733a920df2bb452b4c6
Rojasking700 <grojaspiano@gmail.com>
2021-08-19 14:19:25 -0500

    skill cards in progress

---
+++ src/components/Home/Skills.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="skill">
+        <div class="skill-inner">
+            <div class="skill-text-wrap">
+                <h2 class="bg-text">{{ skill.bgtext }}</h2>
+            </div>
+            <div class="skill-image-wrap">
+                <img class="image" :src="skill.src">
+            </div>
+            <div class="skill-detail">
+                <h2>{{ skill.tittle }}</h2>
+                <p>
+                    <!-- {{ skill.detail }} -->
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime aliquid debitis vero ab veniam. Magnam amet ea laboriosam eius quibusdam?
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'skill',
+    props:['skill']
+}
+</script>
+
+<style>
+    .skill{
+        flex:1;
+        width: 100%;
+        padding: 25px;
+    }
+    .skill-inner{
+        position: relative;
+        padding: 25px;
+        perspective: 1000px;
+    }
+    .skill-text-wrap{
+        position: absolute;
+        top:0;
+        left:0;
+        right:0;
+        bottom:0;
+        z-index: 0;
+        overflow: hidden;
+        perspective: 1000px;
+    }
+    .skill-text-wrap h2{
+        /* color: #FFF; */
+        font-size: 7vw;
+        font-weight: 900;
+        opacity: 0.2;
+        transform-origin: center;
+    }
+    .skill-image-wrap{
+        position:relative;
+        z-index: 1;
+        transform-origin: center;
+    }
+    .skill-image-wrap img {
+        width: 100%;
+        filter: drop-shadow(0px 0px 12px rgba(0,0,0,0,0.25));
+    }
+    .skill-detail{
+        /* color: #FFF; */
+        padding: 25px;
+        margin: 0px -25px -25px;
+    }
+    .skill-detail h2 {
+        font-size: 24vh;
+        font-weight: 700;
+        /* color: #FFF; */
+        margin-bottom: 15px;
+    }
+    .skill-detail p {
+        font-size: 14px;
+        line-height: 1.5;
+        font-weight: 300;
+        /* color: #FFF; */
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,16 @@
     <div class="home">
       <Landing class="Landing"/>
       <Iam class="Iam"/>
+
+      <section class="skills">
+        <Skills  
+          v-for="skill in skills"
+          :key=skill.title
+          :skill="skill"
+          :v-if="skill.Category === 'Technology'"
+        />
+      </section>
+
     </div>
 
   </section>
@@ -12,14 +22,118 @@
 // import gsap from 'gsap'
 import Iam from '../components/Home/Iam.vue';
 import Landing from '../components/Home/Landing.vue';
+import Skills from '../components/Home/Skills.vue';
 
 export default {
   name: 'Home',
   components: {
     Landing,
-    Iam
+    Iam,
+    Skills
+  },
+  data() {
+    return{
+      skills: [
+        {
+          title: 'Allen-Bradley',
+          Category:'Technology',
+          bgtext: 'PLC',
+          src: require('../assets/Skills/Allen-Bradley-Logo.png'),
+          detail: 'stuff'
+        },
+        {
+          title: 'Arduino',
+          Category:'Technology',
+          bgtext: 'Arduino',
+          src: require('../assets/Skills/Arduino-Logo.png'),
+          detail: 'stuff'
+        },
+        {
+          title: 'C#',
+          Category:'Language',
+          bgtext: 'C#',
+          src: require('../assets/Skills/C-Sharp-Logo.png'),
+          detail: 'stuff'
+        },
+        {
+          title: 'CSS',
+          Category:'Language',
+          bgtext: 'CSS',
+          src: require('../assets/Skills/CSS-Logo.png'),
+          detail: 'stuff'
+        },
+        {
+          title: 'Flask',
+          Category:'Framework',
+          bgtext: 'FLASK',
+          src: require('../assets/Skills/Flask-Logo.png'),
+          detail: 'stuff'
+        },
+        {
+          title: 'Google',
+          Category:'Resource',
+          bgtext: 'GOOGLE',
+          src: require('../assets/Skills/Google-Logo.png'),
+          detail: 'stuff'
+        },
+        {
+          title: 'HTML-5',
+          Category:'Language',
+          bgtext: 'HTML5',
+          src: require('../assets/Skills/HTML5-Logo.png'),
+          detail: 'stuff'
+        },
+        {
+          title: 'Javascript',
+          Category:'Language',
+          bgtext: 'JS',
+          src: require('../assets/Skills/Javascript-Logo.png'),
+          detail: 'stuff'
+        },
+        {
+          title: 'Raspberry Pi',
+          Category:'Technology',
+          bgtext: 'RaspPi',
+          src: require('../assets/Skills/Raspberry-pi-Logo.png'),
+          detail: 'stuff'
+        },
+        {
+          title: 'React.js',
+          Category:'Framework',
+          bgtext: 'React',
+          src: require('../assets/Skills/React-Logo.png'),
+          detail: 'stuff'
+        },
+        {
+          title: 'SQL',
+          Category:'Language',
+          bgtext: 'SQL',
+          src: require('../assets/Skills/SQL-Logo.png'),
+          detail: 'stuff'
+        },
+        {
+          title: 'Vue.js',
+          Category:'Framework',
+          bgtext: 'VUE',
+          src: require('../assets/Skills/Vue-Logo.png'),
+          detail: 'stuff'
+        }
+      ]
+    }
   }
 
     
 }
 </script>
+
+<style>
+.skills{
+  display:flex;
+  flex-wrap: wrap;
+  max-width: 70%;
+  padding: 25px;
+  margin: auto;
+}
+
+
+</style>
